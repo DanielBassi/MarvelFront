d94e0c331231a9b0c9b10bc3fa26b694b067f1a2
DanielBassi <danielalbor22@gmail.com>
2025-05-11 01:57:35 -0500

    Se remueve console.log

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -17,7 +17,6 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = this.authService.getUserLoggued();
-    console.log(this.user)
   }
 
   protected logout(): void {
